c71ec9d6998a4e4041a5356a948225d1925e2fdf
Esmaeill <liyanboy74@gmail.com>
2025-06-24 18:03:33 +0330

    more clean code

--- add_all.php
+++ add_all.php
@@ -1,9 +1,10 @@
 <?php
 require 'dbpass.php';
+
 session_start();
 
 // Connect to database
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = @new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
