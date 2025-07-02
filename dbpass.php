a9f7538eead963ffe03473c615d1b118f43bb877
Esmaeill <liyanboy74@gmail.com>
2025-07-02 21:52:39 +0330

    clean code

--- dbpass.php
+++ dbpass.php
@@ -3,9 +3,4 @@ $servername = "localhost";
 $username = "esmaeill";
 $password = "15031374";
 $dbname = "datarain";
-
-// $servername = "localhost";
-// $username = "lapvtpsb_esmaeill";
-// $password = "uj2_T7yc]@6V";
-// $dbname = "lapvtpsb_datarain";
 ?>
